281c852117e711595e99a6872316fa1603096459
Dell <63410077@go.buu.ac.th>
2022-08-10 17:24:19 +0700

    1

---
+++ app/src/main/java/com/example/appformation077/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.appformation077
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.example.appformation077.adapter.ItemAdapter
+import com.example.appformation077.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
